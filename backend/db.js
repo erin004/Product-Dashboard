16395d92393743eb6580955bb0b915595140a865
erin004 <erinbs.ebs@gmail.com>
2026-04-22 16:18:52 +0700

    connect to railway MySQL

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,18 @@
-// koneksi ke database
-
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "product_dashboard"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
-db.connect(err => {
-  if (err) throw err;
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
   console.log("MySQL Connected!");
 });
 
