fdbdeb27e13b2824819fdbd266ecf5c9ce2af0c4
erin004 <erinbs.ebs@gmail.com>
2026-04-22 17:44:30 +0700

    fix: ganti port

--- backend/app.js
+++ backend/app.js
@@ -110,6 +110,8 @@ app.use((req, res) => {
   res.status(404).json({ error: "Endpoint not found" });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
