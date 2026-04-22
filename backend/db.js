435bf230941d93a3dbf754fde669b6f0c04ee327
erin004 <erinbs.ebs@gmail.com>
2026-04-22 17:04:29 +0700

    fix: mysql railway

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   ssl: {
